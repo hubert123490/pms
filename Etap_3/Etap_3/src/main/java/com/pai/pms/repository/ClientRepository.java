f2cc6cdacd89369a4eee15ba8c72d4deda766171
Bzymek Mateusz <mbzymek99@wp.pl>
2021-04-13 20:33:56 +0200

    Stworzenie interfejsów repository z adnotacją @RepositoryRestResource dla wcześniej utworzonych klas.

---
+++ Etap_3/Etap_3/src/main/java/com/pai/pms/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.pai.pms.repository;
+
+import com.pai.pms.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository  extends JpaRepository<Client,Integer> {
+}
